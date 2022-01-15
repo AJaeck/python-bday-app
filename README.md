777c07f6a9445ac8704679703e20fa9db1d6b25e
AJaeck <ajaeck@gmail.com>
2022-01-15 13:01:20 +0100

    flask app initial setup

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # anna-bday-app
+
+## First Steps for Development and Deployment
+
+Source: https://www.digitalocean.com/community/tutorials/how-to-deploy-a-flask-app-using-gunicorn-to-app-platform
+
+1. Create virtual env with:
+   "source ~/.venvs/flask/Scripts/activate"
+
+2. If Development Env necessary, set env variables with:
+   "export FLASK_ENV=development"
+   "export FLASK_APP=app.py"
+
+3. If Deploy necessary, push to heroku with:
+   > Commit repo changes
+   > Login to Heroku: "heroku login"
+   > Push to Heroku: "git push heroku master"
--- app.py
+++ app.py
@@ -1,6 +1,38 @@
-from flask import Flask
+# Flask docs @ https://flask.palletsprojects.com/en/2.0.x/
+from flask import Flask, request, render_template
+
+# Code of your application, which uses environment variables (e.g. from `os.environ` or
+# `os.getenv`) as if they came from the actual environment.
+from dotenv import load_dotenv
+import os
+
+# Sendinblue API Docs: https://developers.sendinblue.com/reference/
+import sib_api_v3_sdk 
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello Sammy!'
+def index():
+    return render_template("index.html")
+
+@app.route('/glueckwunsch')
+def glueckwunsch():
+    return render_template("glueckwunsch.html")
+
+@app.route("/versand", methods=["POST"])
+def form():
+
+    name = request.form.get("name")
+    email = request.form.get("email")
+    gw_text = request.form.get("gw_text")
+    secret = request.form.get("secret")
+
+    #form error hadling
+    if not name or not gw_text or not email or not secret:
+        error_statement = "All Form Fields Required...😢"
+        return render_template("glueckwunsch.html", error_statement=error_statement, name=name, gw_text=gw_text, email=email )
+    
+
+    
+    return render_template("versand.html", name=name)
