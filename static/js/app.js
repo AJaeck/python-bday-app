da760bf1fe146f6b731506d76960af5179aea792
AJaeck <ajaeck@gmail.com>
2022-01-23 13:13:25 +0100

    delay preloader by 1s

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", () => {
-  const preload = document.querySelector(".preload");
-  preload.classList.add("preload-finish");
+  setTimeout(function() {
+    const preload = document.querySelector(".preload");
+    preload.classList.add("preload-finish");
+  }, 1000)
 });
